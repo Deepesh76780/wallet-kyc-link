f45404384cab0f2b4c084be2506cd2545610996b
Deepesh76780 <da76780@gmail.com>
2024-03-09 18:45:14 +0530

    home page for user and CBI

---
+++ app/(user)/adhaar-user/page.tsx
@@ -0,0 +1,9 @@
+import AdhaarApi from "@/components/adhaar-api"
+
+export default function Home() {
+  return (
+      <main className="bg-foreground text-muted">
+        <AdhaarApi />
+      </main>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import AdhaarApi from "@/components/adhaar-api"
 import FetchTrans from "@/components/fetch-transaction"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-      <main className="bg-foreground text-muted">
-        {/* <FetchTrans /> */}
-        <AdhaarApi />
-      </main>
+    <main className="bg-foreground text-muted min-h-screen flex flex-col justify-evenly place-items-center">
+      <h1 className="text-8xl text-primary">Welcome To <span className="text-orange-300">K</span>tr<span className="text-slate-300">Y</span>ack<span className="text-green-300">C</span></h1>
+      <div className="flex gap-8">
+        <Button variant={"secondary"}>
+          User
+        </Button>
+        <Button variant={"secondary"}>
+          CBI
+        </Button>
+      </div>
+      <h1 className="text-muted-foreground">Who are You ?</h1>
+    </main>
   )
 }
 
