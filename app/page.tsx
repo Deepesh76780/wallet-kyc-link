2435a4dfb386eec966864217e2bdbc1b23627161
Deepesh Agrawal <da76780@gmail.com>
2024-04-15 17:00:40 +0530

    refactor header home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,13 @@
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import { BackgroundBeams } from "@/components/backgroundBeams"
+import Header from "@/components/header"
+
 
 export default function Home() {
   return (
-    <main className="bg-foreground text-muted min-h-screen flex flex-col justify-evenly place-items-center">
-      <h1 className="text-8xl text-primary">Welcome To <span className="text-orange-300">K</span>tr<span className="text-slate-300">Y</span>ack<span className="text-green-300">C</span></h1>
-      <div className="flex gap-8">
-        <Link href={"/adhaar-user"}>
-          <Button variant={"secondary"}>
-            User
-          </Button>
-        </Link>
-        <Link href={"/adhaar-admin"}>
-          <Button variant={"secondary"}>
-            CBI
-          </Button>
-        </Link>
-      </div>
-      <h1 className="text-muted-foreground">Who are You ?</h1>
-    </main>
+    <div className="min-h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased gap-[1rem]">
+      <Header/>
+      <BackgroundBeams />
+    </div>
   )
 }
 
