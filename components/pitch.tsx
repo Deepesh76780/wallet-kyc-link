b4892e2226e72b2748f834e454fae3f4e8e256d8
Deepesh Agrawal <da76780@gmail.com>
2024-04-15 17:34:51 +0530

    pitch page

--- components/pitch.tsx
+++ components/pitch.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import { HoverBorderGradient } from './ui/hover-border-gradient'
 
 const Pitch = () => {
     return (
@@ -11,25 +12,31 @@ const Pitch = () => {
                 </h1>
                 <p></p>
                 <p className="text-neutral-500 max-w-lg mx-auto my-5 text-sm text-center relative z-10">
-                    Provides real-time oversight and comprehensive transaction tracking to security agencies and Users to manage there multiple transactions
-                    in one place
+                   Try Now &#x2197;
                 </p>
-                <input
-                    type="text"
-                    placeholder="hi@manuarora.in"
-                    className="rounded-lg border border-neutral-800 p-2 focus:ring-2 text-slate-400 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
-                />
             </div>
-            <div className="flex mt-5 gap-4 mx-auto relative z-10">
+            <div className="flex gap-4 mx-auto relative z-10">
                 <Link href={"/adhaar-user"}>
-                    <Button variant={"secondary"} >
-                        User
-                    </Button>
+                    <div className="flex justify-center text-center ">
+                        <HoverBorderGradient
+                            containerClassName="rounded-full"
+                            as="button"
+                            className="bg-blacktext-white flex items-center space-x-2"
+                        >
+                            <span>User</span>
+                        </HoverBorderGradient>
+                    </div>
                 </Link>
-                <Link href={"/adhaar-admin"} >
-                    <Button variant={"secondary"}>
-                        Admin
-                    </Button>
+                <Link href={"/adhaar-admin"}>
+                    <div className="flex justify-center text-center ">
+                        <HoverBorderGradient
+                            containerClassName="rounded-full"
+                            as="button"
+                            className="bg-blacktext-white flex items-center space-x-2"
+                        >
+                            <span>Admin</span>
+                        </HoverBorderGradient>
+                    </div>
                 </Link>
             </div>
         </div>
